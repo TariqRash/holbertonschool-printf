02b10890788d0e3b7a08fc213895acc1837262bf
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-01 08:21:31 +0000

    Fix:syntax issue add comment

--- print_char.c
+++ print_char.c
@@ -1,5 +1,11 @@
 #include <main.h>
 
+/**
+ * print_char - prints a character
+ * @args: Arguments list
+ * Return: number of characters printed (1)
+ * */
+
 int print_char(va_list args)
 {
 char c = va_args(args; int);
