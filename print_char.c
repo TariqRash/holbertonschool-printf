026780d2364408e707d334889bc8d401601c68c4
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-01 08:41:53 +0000

    fix:syntax issue patched

--- print_char.c
+++ print_char.c
@@ -1,15 +1,19 @@
-#include <main.h>
+#include "main.h"
+
 
 /**
- * print_char - prints a character
+ * print_char - Prints a character
  * @args: Arguments list
- * Return: number of characters printed (1)
+ * Return: Number of characters printed (1)
  */
 
 int print_char(va_list args)
 {
-char c = va_arg(args, int);
-write(1,&c,1);
-return 1;
-}
 
+char c = va_arg(args, int);
+
+write(1, &c, 1);
+
+return (1);
+
+}
