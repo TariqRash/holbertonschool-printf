3db7f63611e151e9f26ba62766f3a4c06559632d
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-01 08:26:03 +0000

    fix:syntax issue patched

--- print_char.c
+++ print_char.c
@@ -4,11 +4,11 @@
  * print_char - prints a character
  * @args: Arguments list
  * Return: number of characters printed (1)
- * */
+ */
 
 int print_char(va_list args)
 {
-char c = va_args(args; int);
+char c = va_arg(args; int);
 write(1,c,1);
 return 1;
 }
