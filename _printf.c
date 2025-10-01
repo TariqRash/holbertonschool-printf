348f2c3d95ae459d7fa51d7ac2e44f7c29b639fa
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-01 10:10:24 +0000

    fix syntax issue patched

--- _printf.c
+++ _printf.c
@@ -10,28 +10,28 @@ int _printf(const char *format, ...)
 {
 
 va_list args,
-int =0, count = 0;
+int i = 0, count = 0;
 
-if(format = NULL)
+if(Format == NULL)
 return -1,
 
 va-start(args, format);
 
 while (format[i])
 {
-if (format[i]= "%")
+if (format[i] == "%")
 {
 i++,
-if (format [i] = "\0")
+if (format [i] == "\0")
 return -1;
 
-if (format[i] = c)
+if (format[i] == c)
 count = print_char(args),
 
-else if (format[i] ="s")
+else if (format[i] == "s")
 count = print_string(args);
 
-else if (format[i} = "%");
+else if (format[i} == "%");
 (
 write (1, "%", 1)
 count+;
