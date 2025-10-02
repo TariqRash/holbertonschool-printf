97935d7d1d2b7f09331df2ceda188ca62716df86
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-02 15:30:53 +0000

    print_string.c

---
+++ print_string.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+/**
+ * print_string - prints a string
+ * @args: Arguments list
+ * rutern:number of characters printed
+ * */
+
+int print string(va-list args)
+
+	char *str = va_arge(args, chsr)
+	int i =0 
+	if (str = null)
+	str = "(null)";
+
+
+	while (str[i])
+	(
+	 write(1,str[i],1);
+	 i++;
+	 }
+
+	 return i;
+
