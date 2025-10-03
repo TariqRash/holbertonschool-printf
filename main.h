3e3b6e9c774803808bff1a52aa3e5943fe5f5279
root <root@ip-10-42-90-179.ec2.internal>
2025-10-03 05:52:39 +0000

    style: fix Betty violations - split long function and fix spacing

--- main.h
+++ main.h
@@ -5,7 +5,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int _printf(const char *format,...);
+int _printf(const char *format, ...);
+int handle_specifier(char format, va_list args);
 int print_char(va_list args);
 int print_string(va_list args);
 int print_number(va_list args);
