bfd2ed09910dd3a1aa51d00fbb430e3f2e36c0b3
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-03 21:19:31 +0000

    fix: simplify _printf to remove spec parsing bugs causing text loss (Task 5)

--- _printf.c
+++ _printf.c
@@ -1,101 +1,61 @@
 #include "main.h"
 
-int handle_specifier(char format, va_list args, format_t spec)
-{
-	int count = 0;
-
-	if (format == 'c')
-		count += print_char(args);
-	else if (format == 's')
-		count += print_string(args);
-	else if (format == 'S')
-		count += print_custom_string(args);
-	else if (format == '%')
-	{
-		add_to_buffer('%');
-		count++;
-	}
-	else if (format == 'd' || format == 'i')
-	{
-		if (spec.flags || spec.length)
-			count += print_number_spec(args, spec);
-		else
-			count += print_number(args);
-	}
-	else if (format == 'u')
-	{
-		if (spec.length)
-			count += print_unsigned_spec(args, spec);
-		else
-			count += print_unsigned(args);
-	}
-	else if (format == 'o')
-	{
-		if (spec.flags || spec.length)
-			count += print_octal_spec(args, spec);
-		else
-			count += print_octal(args);
-	}
-	else if (format == 'x')
-	{
-		if (spec.flags || spec.length)
-			count += print_hex_lower_spec(args, spec);
-		else
-			count += print_hex_lower(args);
-	}
-	else if (format == 'X')
-	{
-		if (spec.flags || spec.length)
-			count += print_hex_upper_spec(args, spec);
-		else
-			count += print_hex_upper(args);
-	}
-	else if (format == 'b')
-		count += print_binary(args);
-	else if (format == 'p')
-		count += print_pointer(args);
-	else if (format == 'r')
-		count += print_reverse(args);
-	else if (format == 'R')
-		count += print_rot13(args);
-	else
-	{
-		add_to_buffer('%');
-		add_to_buffer(format);
-		count += 2;
-	}
-	return (count);
-}
-
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i;
-	int count;
-	format_t spec;
+	int i, count = 0;
 
 	if (format == NULL)
 		return (-1);
 	reset_buffer();
 	va_start(args, format);
-	i = 0;
-	count = 0;
-	while (format[i])
+	for (i = 0; format[i]; i++)
 	{
-		if (format[i] != '%')
+		if (format[i] == '%')
 		{
-			add_to_buffer(format[i]);
-			count++;
 			i++;
+			if (!format[i])
+				break;
+			if (format[i] == 'c')
+				count += print_char(args);
+			else if (format[i] == 's')
+				count += print_string(args);
+			else if (format[i] == 'S')
+				count += print_custom_string(args);
+			else if (format[i] == '%')
+			{
+				add_to_buffer('%');
+				count++;
+			}
+			else if (format[i] == 'd' || format[i] == 'i')
+				count += print_number(args);
+			else if (format[i] == 'u')
+				count += print_unsigned(args);
+			else if (format[i] == 'o')
+				count += print_octal(args);
+			else if (format[i] == 'x')
+				count += print_hex_lower(args);
+			else if (format[i] == 'X')
+				count += print_hex_upper(args);
+			else if (format[i] == 'b')
+				count += print_binary(args);
+			else if (format[i] == 'p')
+				count += print_pointer(args);
+			else if (format[i] == 'r')
+				count += print_reverse(args);
+			else if (format[i] == 'R')
+				count += print_rot13(args);
+			else
+			{
+				add_to_buffer('%');
+				add_to_buffer(format[i]);
+				count += 2;
+			}
 		}
 		else
 		{
-			i++;
-			if (format[i] == '\0')
-				break;
-			spec = parse_flags(format, &i);
-			count += handle_specifier(format[i], args, spec);
-			i++;
+			add_to_buffer(format[i]);
+			count++;
 		}
 	}
 	va_end(args);
