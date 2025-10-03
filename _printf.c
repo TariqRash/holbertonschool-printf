8cb046301ea53da53ad778bc4da6ce8d95f7ad8a
root <root@ip-10-42-90-179.ec2.internal>
2025-10-03 05:51:08 +0000

    fix: correct all syntax errors in _printf function

--- _printf.c
+++ _printf.c
@@ -2,57 +2,53 @@
 
 /**
  * _printf - Custom printf function
- * @format: Format string
- * Return : Number of characters printed
+ * @format: Format string containing conversion specifiers
+ *
+ * Return: Number of characters printed, -1 on error
  */
-
 int _printf(const char *format, ...)
 {
+	va_list args;
+	int i = 0, count = 0;
 
-va_list args,
-int i = 0, count = 0;
+	if (format == NULL)
+		return (-1);
 
-if (Format == NULL)
-return (-1),
+	va_start(args, format);
 
-va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+				return (-1);
 
-while (format[i])
-{
-if (format[i] == '%')
-{
-i++,
-if (format [i] == '\0')
-return (-1);
-
-if (format[i] == 'c')
-count += print_char(args);
-
-else if (format[i] == 's')
-count += print_string(args);
-else if (format[i] == '%')
-{
-write (1, "%", 1);
-count++;
-}
-
-else if (format[i] == 'd'  || format[i] == 'i')
-count += print_number(args);
-else 
-{
-write (1, "%" ,1);
-write(1, &format[i], 1);
-count+=2;
-}
-}
-else 
-{
-write(1, &format[i], 1);
-count++;
-}
-i++;
-
-}
-va_end(args),
-return (count),
+			if (format[i] == 'c')
+				count += print_char(args);
+			else if (format[i] == 's')
+				count += print_string(args);
+			else if (format[i] == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
+			else if (format[i] == 'd' || format[i] == 'i')
+				count += print_number(args);
+			else
+			{
+				write(1, "%", 1);
+				write(1, &format[i], 1);
+				count += 2;
+			}
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
 }
