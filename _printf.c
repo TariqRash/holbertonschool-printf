d5a94e0072f92119b55abd24c5ed2077df825dde
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-03 10:41:40 +0000

    fix: resolve syntax errors and improve documentation in _printf

--- _printf.c
+++ _printf.c
@@ -1,53 +1,6 @@
 #include "main.h"
 
 /**
- * handle_specifier - Handle format specifier
- * @format: Format character
- * @args: Arguments list
- *
- * Return: Number of characters printed
- */
-int handle_specifier(char format, va_list args)
-{
-	int count = 0;
-
-	if (format == 'c')
-		count += print_char(args);
-	else if (format == 's')
-		count += print_string(args);
-	else if (format == '%')
-	{
-		write(1, "%", 1);
-		count++;
-	}
-	else if (format == 'd' || format == 'i')
-		count += print_number(args);
-	else if (format == 'b')
-		count += print_binary(args);
-	else if (format == 'u')
-		count += print_unsigned(args);
-	else if (format == 'o')
-		count += print_octal(args);
-	else if (format == 'x')
-		count += print_hex_lower(args);
-	else if (format == 'X')
-		count += print_hex_upper(args);
-	else if (format == 'p')
-		count += print_pointer(args);
-	else if (format == 'r')
-		count += print_reverse(args);
-	else if (format == 'R')
-		count += print_rot13(args);
-	else
-	{
-		write(1, "%", 1);
-		write(1, &format, 1);
-		count += 2;
-	}
-	return (count);
-}
-
-/**
  * _printf - Custom printf function
  * @format: Format string containing conversion specifiers
  *
@@ -60,7 +13,9 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
+
 	va_start(args, format);
+
 	while (format[i])
 	{
 		if (format[i] == '%')
@@ -68,7 +23,24 @@ int _printf(const char *format, ...)
 			i++;
 			if (format[i] == '\0')
 				return (-1);
-			count += handle_specifier(format[i], args);
+
+			if (format[i] == 'c')
+				count += print_char(args);
+			else if (format[i] == 's')
+				count += print_string(args);
+			else if (format[i] == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
+			else if (format[i] == 'd' || format[i] == 'i')
+				count += print_number(args);
+			else
+			{
+				write(1, "%", 1);
+				write(1, &format[i], 1);
+				count += 2;
+			}
 		}
 		else
 		{
