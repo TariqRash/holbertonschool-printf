02c2e8b0bbdfce51fb868a0b2df41e895d8ab26e
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-03 11:16:13 +0000

    main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+    _printf("Hello, world!\n");
+    _printf("Number: %d\n", 123);
+    _printf("Char: %c\n", 'A');
+    _printf("String: %s\n", "Test string");
+
+    return 0;
+}
