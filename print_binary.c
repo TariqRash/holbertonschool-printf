4d4009af31181dff2cfb36245ab34c5c39d9f678
root <root@ip-10-42-117-54.ec2.internal>
2025-10-03 21:55:10 +0000

    fix: use signed int for loop counter in print_binary

--- print_binary.c
+++ print_binary.c
@@ -1,8 +1,32 @@
 #include "main.h"
+
 int print_binary(va_list args)
 {
-	unsigned int n = va_arg(args, unsigned int), mask, i = 31, started = 0, count = 0;
-	if (n == 0) { write(1, "0", 1); return (1); }
-	for (; i >= 0 && i < 32; i--) { mask = 1u << i; if (n & mask) { write(1, "1", 1); count++; started = 1; } else if (started) { write(1, "0", 1); count++; } }
+	unsigned int n = va_arg(args, unsigned int);
+	unsigned int mask;
+	int i;
+	int started = 0;
+	int count = 0;
+
+	if (n == 0)
+	{
+		write(1, "0", 1);
+		return (1);
+	}
+	for (i = 31; i >= 0; i--)
+	{
+		mask = 1u << i;
+		if (n & mask)
+		{
+			write(1, "1", 1);
+			count++;
+			started = 1;
+		}
+		else if (started)
+		{
+			write(1, "0", 1);
+			count++;
+		}
+	}
 	return (count);
 }
