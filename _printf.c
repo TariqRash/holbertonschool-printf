5de02d1405d0bece680caf6fbeecacb6e7ef0f21
root <root@ip-10-42-117-54.ec2.internal>
2025-10-03 22:04:54 +0000

    fix: print invalid format sequences literally (% edge case)

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i = 0, count = 0, start_pos;
+	int i = 0, count = 0, j;
 	format_t spec;
 
 	if (format == NULL)
@@ -13,31 +13,21 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			start_pos = i;
+			j = i;
 			i++;
 			if (format[i] == '\0')
-			{
-				write(1, "%", 1);
-				count++;
-				break;
-			}
+				return (-1);
 			spec = parse_flags(format, &i);
-			if (format[i] == '\0' || (format[i] != 'c' && format[i] != 's' && 
-			    format[i] != 'S' && format[i] != '%' && format[i] != 'd' && 
-			    format[i] != 'i' && format[i] != 'u' && format[i] != 'o' && 
-			    format[i] != 'x' && format[i] != 'X' && format[i] != 'b' && 
-			    format[i] != 'p' && format[i] != 'r' && format[i] != 'R'))
-			{
-				while (start_pos <= i)
-				{
-					write(1, &format[start_pos], 1);
-					count++;
-					start_pos++;
-				}
-			}
+			if (format[i] == 'c' || format[i] == 's' || format[i] == 'S' ||
+			    format[i] == '%' || format[i] == 'd' || format[i] == 'i' ||
+			    format[i] == 'u' || format[i] == 'o' || format[i] == 'x' ||
+			    format[i] == 'X' || format[i] == 'b' || format[i] == 'p' ||
+			    format[i] == 'r' || format[i] == 'R')
+				count += handle_specifier(format[i], args, spec);
 			else
 			{
-				count += handle_specifier(format[i], args, spec);
+				while (j <= i)
+					count += write(1, &format[j++], 1);
 			}
 		}
 		else
