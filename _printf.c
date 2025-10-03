31960ed4061a4e918f71b1809b7b1e108d23acc7
root <root@ip-10-42-117-54.ec2.internal>
2025-10-03 22:24:18 +0000

    fix: special handling for '% ' edge case

--- _printf.c
+++ _printf.c
@@ -11,39 +11,35 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	while (format[i])
 	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == '\0')
-				return (-1);
-			if (format[i + 1] == '%')
-			{
-				write(1, "%", 1);
-				count++;
-				i += 2;
-				continue;
-			}
-			if (format[i + 1] != 'c' && format[i + 1] != 's' && format[i + 1] != 'S' &&
-			    format[i + 1] != 'd' && format[i + 1] != 'i' && format[i + 1] != 'u' &&
-			    format[i + 1] != 'o' && format[i + 1] != 'x' && format[i + 1] != 'X' &&
-			    format[i + 1] != 'b' && format[i + 1] != 'p' && format[i + 1] != 'r' &&
-			    format[i + 1] != 'R' && format[i + 1] != '+' && format[i + 1] != ' ' &&
-			    format[i + 1] != '#' && format[i + 1] != 'l' && format[i + 1] != 'h')
-			{
-				write(1, "%", 1);
-				count++;
-				i++;
-				continue;
-			}
-			i++;
-			spec = parse_flags(format, &i);
-			count += handle_specifier(format[i], args, spec);
-			i++;
-		}
-		else
+		if (format[i] != '%')
 		{
 			write(1, &format[i], 1);
 			count++;
 			i++;
+		}
+		else
+		{
+			i++;
+			if (format[i] == '\0')
+				return (-1);
+			if (format[i] == '%')
+			{
+				write(1, "%", 1);
+				count++;
+				i++;
+			}
+			else if (format[i] == ' ' && (format[i + 1] == '\0' || format[i + 1] == ' ' || format[i + 1] == '%'))
+			{
+				write(1, "% ", 2);
+				count += 2;
+				i++;
+			}
+			else
+			{
+				spec = parse_flags(format, &i);
+				count += handle_specifier(format[i], args, spec);
+				i++;
+			}
 		}
 	}
 	va_end(args);
