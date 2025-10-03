7787399da9ae60df5345898a8b8865e7cb9acfdd
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-03 10:42:30 +0000

    docs: improve documentation for print_char function

--- print_char.c
+++ print_char.c
@@ -1,19 +1,15 @@
 #include "main.h"
 
-
 /**
- * print_char - Prints a character
- * @args: Arguments list
- * Return: Number of characters printed (1)
+ * print_char - Prints a single character
+ * @args: Arguments list containing the character
+ *
+ * Return: Number of characters printed (always 1)
  */
-
 int print_char(va_list args)
 {
+	char c = va_arg(args, int);
 
-char c = va_arg(args, int);
-
-write(1, &c, 1);
-
-return (1);
-
+	write(1, &c, 1);
+	return (1);
 }
