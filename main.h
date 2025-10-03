c1d1ac5f16bcae0382375399985c98f1254f1b2a
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-03 10:43:59 +0000

    refactor: ensure all function prototypes are properly declared

--- main.h
+++ main.h
@@ -1,23 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include 
+#include 
+#include 
 
 int _printf(const char *format, ...);
-int handle_specifier(char format, va_list args);
 int print_char(va_list args);
 int print_string(va_list args);
 int print_number(va_list args);
 int print_int(int n);
-int print_binary(va_list args);
-int print_unsigned(va_list args);
-int print_octal(va_list args);
-int print_hex_lower(va_list args);
-int print_hex_upper(va_list args);
-int print_pointer(va_list args);
-int print_reverse(va_list args);
-int print_rot13(va_list args);
 
 #endif
